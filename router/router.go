74adfb0128f3527aa585fc00ebfda00d1a53669e
jay-bhogayata <jaybhogayata53@gmail.com>
2024-02-05 19:57:08 +0530

    feat: Add logger middleware and update gitignore

--- router/router.go
+++ router/router.go
@@ -3,11 +3,14 @@ package router
 import (
 	"github.com/go-chi/chi/v5"
 	"github.com/jay-bhogayata/blogapi/handlers"
+	"github.com/jay-bhogayata/blogapi/middleware"
 )
 
 func NewRouter(h *handlers.Handlers) *chi.Mux {
 
 	r := chi.NewRouter()
+
+	r.Use(middleware.Logging)
 
 	r.Route("/api/v1", func(r chi.Router) {
 		r.Get("/health", h.CheckHealth)
