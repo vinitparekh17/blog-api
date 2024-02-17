60beff1181c85d6dfa9a0fe6b8bee88571901008
jay-bhogayata <jaybhogayata53@gmail.com>
2024-02-17 19:40:20 +0530

    refactor: Add response struct and update RegisterUser and LoginUser handlers

--- handlers/account.go
+++ handlers/account.go
@@ -21,6 +21,10 @@ type User struct {
 	Email    string      `json:"email"`
 }
 
+type Response struct {
+	Message string `json:"message"`
+}
+
 func (h *Handlers) RegisterUser(w http.ResponseWriter, r *http.Request) {
 	type requestUser struct {
 		Username          string      `json:"username"`
@@ -68,9 +72,10 @@ func (h *Handlers) RegisterUser(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusCreated)
-	h.respondWithJSON(w, http.StatusCreated, fmt.Sprintf("User created with id: %v", usr.UserID))
+	msg := fmt.Sprintf("Account created successfully. Verification link has been sent to %s", usr.Email)
+	res := Response{Message: msg}
+
+	h.respondWithJSON(w, http.StatusCreated, res)
 }
 
 func (h *Handlers) GenerateToken() (token string, err error) {
@@ -157,9 +162,8 @@ func (h *Handlers) LoginUser(w http.ResponseWriter, r *http.Request) {
 		Path:     "/",
 		HttpOnly: true,
 	})
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	h.respondWithJSON(w, http.StatusOK, "Logged in successfully")
+
+	h.respondWithJSON(w, http.StatusOK, &Response{Message: "Logged in successfully"})
 }
 
 func (h *Handlers) LogoutUser(w http.ResponseWriter, r *http.Request) {
@@ -171,9 +175,8 @@ func (h *Handlers) LogoutUser(w http.ResponseWriter, r *http.Request) {
 		HttpOnly: true,
 		Expires:  time.Unix(0, 0),
 	})
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	h.respondWithJSON(w, http.StatusOK, "Logged out successfully")
+
+	h.respondWithJSON(w, http.StatusOK, &Response{Message: "Logged out successfully"})
 }
 
 func (h *Handlers) GetUserInfoByUserEmail(w http.ResponseWriter, r *http.Request) {
@@ -203,9 +206,7 @@ func (h *Handlers) GetAllUsers(w http.ResponseWriter, r *http.Request) {
 		h.respondWithError(w, http.StatusInternalServerError, err.Error())
 		return
 	}
-
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 	w.Write(res)
-
 }
