119a351563072c5dae4c694e6be56c423414bf26
jay-bhogayata <jaybhogayata53@gmail.com>
2024-02-29 20:54:07 +0530

    refactor: error handling and logging in category handlers

--- handlers/categories.go
+++ handlers/categories.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/go-chi/chi/v5"
 	"github.com/jay-bhogayata/blogapi/database"
+	"github.com/jay-bhogayata/blogapi/internal/helper"
 )
 
 type ErrorResponse struct {
@@ -20,7 +21,8 @@ type Category struct {
 func (h *Handlers) GetAllCategories(w http.ResponseWriter, r *http.Request) {
 	categories, err := h.query.GetAllCategories(r.Context())
 	if err != nil {
-		h.respondWithError(w, http.StatusInternalServerError, err.Error())
+		h.logger.Error("error while fetching categories: ", "error", err.Error())
+		h.respondWithError(w, http.StatusInternalServerError, "error while fetching categories")
 		return
 	}
 
@@ -30,17 +32,19 @@ func (h *Handlers) GetAllCategories(w http.ResponseWriter, r *http.Request) {
 func (h *Handlers) GetCategoryByID(w http.ResponseWriter, r *http.Request) {
 	id, err := h.ParseID(chi.URLParam(r, "id"))
 	if err != nil {
-		h.respondWithError(w, http.StatusBadRequest, err.Error())
+		h.logger.Error("invalid category id", "error", err.Error())
+		h.respondWithError(w, http.StatusBadRequest, "invalid category id")
 		return
 	}
 
 	category, err := h.query.GetCategoryById(r.Context(), id)
 	if err != nil {
 		if err.Error() == "no rows in result set" {
+			h.logger.Error("no category found with given id", "error", err.Error())
 			h.respondWithJSON(w, http.StatusNotFound, &ErrorResponse{Message: "No category found with given id"})
 			return
 		}
-		h.respondWithError(w, http.StatusInternalServerError, err.Error())
+		h.respondWithError(w, http.StatusInternalServerError, http.StatusText(http.StatusInternalServerError))
 		return
 	}
 
@@ -49,14 +53,18 @@ func (h *Handlers) GetCategoryByID(w http.ResponseWriter, r *http.Request) {
 
 func (h *Handlers) CreateCategory(w http.ResponseWriter, r *http.Request) {
 	var category Category
-	if err := json.NewDecoder(r.Body).Decode(&category); err != nil {
+
+	err := helper.DecodeJSONBody(w, r, &category)
+	if err != nil {
+		h.logger.Error("error while decoding request body", "error", err.Error())
 		h.respondWithError(w, http.StatusBadRequest, err.Error())
 		return
 	}
 
 	id, err := h.query.CreateCategory(r.Context(), category.Name)
 	if err != nil {
-		h.respondWithError(w, http.StatusInternalServerError, err.Error())
+		h.logger.Error("error while creating category", "error", err.Error())
+		h.respondWithError(w, http.StatusInternalServerError, http.StatusText(http.StatusInternalServerError))
 		return
 	}
 
@@ -65,14 +73,17 @@ func (h *Handlers) CreateCategory(w http.ResponseWriter, r *http.Request) {
 
 func (h *Handlers) UpdateCategory(w http.ResponseWriter, r *http.Request) {
 	var category database.UpdateCategoryParams
-	if err := json.NewDecoder(r.Body).Decode(&category); err != nil {
+	err := helper.DecodeJSONBody(w, r, &category)
+	if err != nil {
+		h.logger.Error("error while decoding request body", "error", err.Error())
 		h.respondWithError(w, http.StatusBadRequest, err.Error())
 		return
 	}
 
 	updatedCategory, err := h.query.UpdateCategory(r.Context(), category)
 	if err != nil {
-		h.respondWithError(w, http.StatusInternalServerError, err.Error())
+		h.logger.Error("error while updating category", "error", err.Error())
+		h.respondWithError(w, http.StatusInternalServerError, http.StatusText(http.StatusInternalServerError))
 		return
 	}
 
@@ -82,7 +93,8 @@ func (h *Handlers) UpdateCategory(w http.ResponseWriter, r *http.Request) {
 func (h *Handlers) DeleteCategory(w http.ResponseWriter, r *http.Request) {
 	id, err := h.ParseID(chi.URLParam(r, "id"))
 	if err != nil {
-		h.respondWithError(w, http.StatusBadRequest, err.Error())
+		h.logger.Error("invalid category id", "error", err.Error())
+		h.respondWithError(w, http.StatusBadRequest, "invalid category id")
 		return
 	}
 
@@ -92,6 +104,7 @@ func (h *Handlers) DeleteCategory(w http.ResponseWriter, r *http.Request) {
 
 	if _, err := h.query.GetCategoryById(r.Context(), id); err != nil {
 		if err.Error() == "no rows in result set" {
+			h.logger.Error("no category found with given id", "error", err.Error())
 			h.respondWithJSON(w, http.StatusNotFound, &ErrorResponse{Message: "No category found with given id"})
 			return
 		}
@@ -100,7 +113,8 @@ func (h *Handlers) DeleteCategory(w http.ResponseWriter, r *http.Request) {
 	}
 
 	if err := h.query.DeleteCategory(r.Context(), id); err != nil {
-		h.respondWithError(w, http.StatusInternalServerError, err.Error())
+		h.logger.Error("error while deleting category", "error", err.Error())
+		h.respondWithError(w, http.StatusInternalServerError, http.StatusText(http.StatusInternalServerError))
 		return
 	}
 
